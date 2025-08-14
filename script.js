3c1f2416f5efea3ec3008824f2a78e100242723e
yousef-sheha12 <sheha8757@gmail.com>
2025-08-14 22:01:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('cl');
+const btnn = document.getElementById('cll');
+ const click = btn.addEventListener('click',function () {
+alert(' 🥷🧑‍💻Developer ملكش دعوه بقولك ');
+});
+ const clicks = btnn.addEventListener('click',function () {
+alert(' 🥷🧑‍💻Developer يجدع بقولك ');
+});
